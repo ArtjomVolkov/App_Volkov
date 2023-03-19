00e4df2959dd758238b51686717af85b5fb366d5
Артём Минидогов <mr.pon4ik337@mail.ru>
2023-03-19 15:27:57 +0200

    Update

--- App_Volkov/App_Volkov/Aiaplaan.xaml.cs
+++ App_Volkov/App_Volkov/Aiaplaan.xaml.cs
@@ -1,62 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace App_Volkov
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Aiaplaan : ContentPage
     {
-        private List<Note> notes;
+        private List<Task> tasks;
 
         public Aiaplaan()
         {
             InitializeComponent();
 
-            notes = new List<Note>();
+            tasks = new List<Task>();
 
-            for (int i = 0; i < 12; i++)
+            for (int i = 1; i <= 12; i++)
             {
-                notes.Add(new Note { ImageSource = $"img{i}.jpg", Name = $"Note {i + 1}" });
+                tasks.Add(new Task
+                {
+                    Image = $"img{i}.jpg",
+                    Taska = $"Задача {i}",
+                    Time = new System.TimeSpan(8 + i, 0, 0),
+                    Completed = 0,
+                    Description = $"Описание задачи {i}"
+                });
             }
 
-            listView.ItemsSource = notes;
-
-            listView.ItemTapped += OnItemTapped;
+            myListView.ItemsSource = tasks;
+            myListView.ItemTapped += OnListViewItemTapped;
         }
 
-        private void OnItemTapped(object sender, ItemTappedEventArgs e)
+        private void SaveTasks_Clicked(object sender, System.EventArgs e)
         {
-            var note = (Note)e.Item;
-
-            DisplayAlert("Note", $"You tapped {note.Name}", "OK");
+            Application.Current.Properties["Tasks"] = Newtonsoft.Json.JsonConvert.SerializeObject(tasks);
         }
-
-        private void OnTimePickerPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private async void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
         {
-            var timePicker = (TimePicker)sender;
-
-            if (timePicker.BindingContext is Note note)
+            if (e.Item != null)
             {
-                var hour = timePicker.Time.Hours;
-
-                note.ImageSource = $"img{hour}.jpg";
-                note.Name = $"Note {hour + 1}";
-
-                listView.ItemsSource = null;
-                listView.ItemsSource = notes;
+                var task = (Task)e.Item;
+                await DisplayAlert("Описание задачи", task.Description, "OK");
+                myListView.SelectedItem = null;
             }
         }
-        public class Note
-        {
-            public string ImageSource { get; set; }
-            public string Name { get; set; }
-        }
+    }
+
+    public class Task
+    {
+        public string Image { get; set; }
+        public string Taska { get; set; }
+        public System.TimeSpan Time { get; set; }
+        public int Completed { get; set; }
+        public string Description { get; set; }
     }
 }
